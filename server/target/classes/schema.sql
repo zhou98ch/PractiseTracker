cccaa66168dca5b8f9f84345eafbdac3cde2055c
zhou98ch <jingyuezhou@outlook.com>
2025-08-20 17:39:52 +0200

    Remove target folders

--- server/target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS practice_time_record (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(255),
-    music_id VARCHAR(255),
-    bpm INT,
-    date DATE,
-    duration BIGINT,
-    updated_at TIMESTAMP
-);
